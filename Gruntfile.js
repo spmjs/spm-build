fb7d6578326a968dac16e7cb408842cfa65cb94d
Hsiaoming Yang <lepture@me.com>
2013-01-30 14:13:15 +0800

    init

---
+++ Gruntfile.js
@@ -0,0 +1,81 @@
+/*
+ * grunt-spm-build
+ * https://github.com/spmjs/grunt-spm-build
+ *
+ * Copyright (c) 2013 Hsiaoming Yang
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp'],
+    },
+
+    'spm-transport': {
+      options: {
+        paths: ["sea-modules"],
+        src: 'test/fixtures',
+        dest: 'tmp'
+      },
+      jsfiles: ['test/fixtures/*.js'],
+      cssfiles: ['test/fixtures/*.css']
+    },
+
+    // Configuration to be run (and then tested).
+    spm_build: {
+      default_options: {
+        options: {
+        },
+        files: {
+          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+      custom_options: {
+        options: {
+          separator: ': ',
+          punctuation: ' !!!',
+        },
+        files: {
+          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js'],
+    },
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'spm_build', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/transport.js
@@ -0,0 +1,45 @@
+/*
+ * grunt-spm-build
+ * https://github.com/spmjs/grunt-spm-build
+ *
+ * Copyright (c) 2013 Hsiaoming Yang
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('spm-transport', 'transport everything to js.', function() {
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(fileObj) {
+      // The source files to be concatenated. The "nonull" option is used
+      // to retain invalid files/patterns so they can be warned about.
+      var files = grunt.file.expand({nonull: true}, fileObj.src);
+
+      // Concat specified files.
+      var src = files.map(function(filepath) {
+        // Warn if a source file/pattern was invalid.
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.error('Source file "' + filepath + '" not found.');
+          return '';
+        }
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(options.separator);
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(fileObj.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + fileObj.dest + '" created.');
+    });
+  });
+
+};
