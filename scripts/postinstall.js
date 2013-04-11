467d4557ca084d65d83867111e616b5290b36b5c
Hsiaoming Yang <me@lepture.com>
2013-04-11 15:31:17 +0800

    it works

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -5,12 +5,12 @@ try {
   spm.plugin.install({
     name: 'build',
     bin: 'spm-build',
-    description: 'The best spm plugin ever.'
+    description: 'Build a standar cmd module.'
   });
 } catch(e) {
   console.log('  you need install spm to register the program');
   console.log();
-  console.log('    \x1b[31m$ npm install spm@~2.0.0 -g\x1b[0m');
+  console.log('    \x1b[31m$ npm install spm -g\x1b[0m');
   console.log();
   console.log("  if you have installed spm, it maybe you haven't set a NODE_PATH environment variable");
   console.log();
