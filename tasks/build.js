28c1c7078a87dfb15cfb14504f7b46f139e2fd3e
popomore <sakura9515@gmail.com>
2013-06-24 20:35:23 +0800

    force option for spm.install

--- tasks/build.js
+++ tasks/build.js
@@ -4,9 +4,15 @@ module.exports = function(grunt) {
   // register spm install tasks
   grunt.registerTask('spm-install', function() {
     var done = this.async();
+    var options = this.options({
+      force: false
+    });
     try {
       var spm = require('spm');
-      spm.install({query: []}, done);
+      spm.install({
+        query: [],
+        force: options.force
+      }, done);
     } catch (e) {
       grunt.log.warn('spm ' + e.message || e);
       done();
