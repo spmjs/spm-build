d4c464d9931598e5bb31752830b3578ed39c77ca
unknown <zhiying.mai@gmail.com>
2013-08-26 16:57:13 +0800

    Customize build

--- tasks/build.js
+++ tasks/build.js
@@ -5,7 +5,8 @@ module.exports = function(grunt) {
   grunt.registerTask('spm-install', function() {
     var done = this.async();
     var options = this.options({
-      force: false
+      force: false,
+      destination: ""
     });
 
     var spm;
@@ -18,7 +19,8 @@ module.exports = function(grunt) {
     if (spm) {
       spm.install({
         query: [],
-        force: options.force
+        force: options.force,
+        destination: options.dest.match(/^.*sea-modules/) ? options.dest.match(/^.*sea-modules/)[0] : undefined
       }, function(err) {
         if (err) {
           console.log();
